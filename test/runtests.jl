486fec9496d15d54c6bf65a0ff32bc6076504cdf
Alister Trabattoni <alister.trabattoni@gmail.com>
2020-04-15 18:29:40 +0200

    Reorganized tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,29 +3,32 @@ using Test
 
 @testset "Obsea.jl" begin
 
-@testset "State, Trajectory, Metadata and Particle structures" begin
-
+@testset "Structures" begin
     state = State(1, 2.0, 3.0, 4.0, 5.0, 6.0)
-    @test state.model === 1
-    @test state.frequency === 2.0
-    @test state.x === 3.0
-    @test state.y === 4.0
-    @test state.vx === 5.0
-    @test state.vy === 6.0
-
     trajectory = Trajectory([state, state])
-    @test trajectory == [state, state]
-    @test trajectory[1] === state
-
     metadata = Metadata(0.1)
-    @test metadata.weight === 0.1
-
     particle = Particle(trajectory, metadata)
-    @test particle.trajectory === trajectory
-    @test particle.metadata === metadata
-    particle.metadata.weight *= 2.0
-    @test particle.metadata.weight === 0.2
-
+    @testset "State" begin
+        @test state.model === 1
+        @test state.frequency === 2.0
+        @test state.x === 3.0
+        @test state.y === 4.0
+        @test state.vx === 5.0
+        @test state.vy === 6.0
+    end
+    @testset "Trajectory" begin
+        @test trajectory == [state, state]
+        @test trajectory[1] === state
+    end
+    @testset "Metadata" begin
+        @test metadata.weight === 0.1
+    end
+    @testset "Particle" begin
+        @test particle.trajectory === trajectory
+        @test particle.metadata === metadata
+        particle.metadata.weight *= 2.0
+        @test particle.metadata.weight === 0.2
+    end
 end
 
 @testset "Motion Model" begin
