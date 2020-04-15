1cdc328de091fbef270f7e9b950d282fef7ec2b3
Alister Trabattoni <alister.trabattoni@gmail.com>
2020-04-15 15:48:51 +0200

    Added State structure.

--- src/Obsea.jl
+++ src/Obsea.jl
@@ -1,5 +1,14 @@
 module Obsea
 
-greet() = print("Hello World!")
+export State
+
+struct State
+    m::Int64
+    x::Float64
+    y::Float64
+    vx::Float64
+    vy::Float64
+    f::Float64
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using Obsea
 using Test
 
-@testset "Obsea.jl" begin
-    # Write your own tests here.
+@testset "State" begin
+    s = State(1, 2.0, 3.0, 4.0, 5.0, 6.0)
+    @test s.m === 1
+    @test s.x === 2.0
+    @test s.y === 3.0
+    @test s.vx === 4.0
+    @test s.vy === 5.0
 end
