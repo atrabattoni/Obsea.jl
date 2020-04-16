df5eb052cbbae0f9110ecbafa4dc82e9dcb8b3a1
Alister Trabattoni <alister.trabattoni@gmail.com>
2020-04-16 16:53:18 +0200

    Reorganization bis

--- src/predict.jl
+++ src/predict.jl
@@ -1,3 +1,26 @@
+function logf(state::State, prevstate::State, params::Parameters)
+    q, T, ps = params.q, params.T, params.ps
+    dvx = state.vx - prevstate.vx
+    dvy = state.vy - prevstate.vy
+    log(ps) - (dvx^2 + dvy^2) / (q * T)^2  # TODO
+end
+
+function logf(state::EmptyState, prevstate::State, params::Parameters)
+    ps = params.ps
+    log(1.0 - ps)
+end
+
+function logf(state::State, prevstate::EmptyState, params::Parameters)
+    pb = params.pb
+    log(pb)
+end
+
+function logf(state::EmptyState, prevstate::EmptyState, params::Parameters)
+    pb = params.pb
+    log(1.0 - pb)
+end
+
+
 function transition(state::State, scan::Scan, params::Parameters)
     if rand() < params.ps
         return move(state::State, params::Parameters)
@@ -5,7 +28,6 @@ function transition(state::State, scan::Scan, params::Parameters)
         return EmptyState()
     end
 end
-
 
 function transition(state::EmptyState, scan::Scan, params::Parameters)
     if rand() < params.pb
