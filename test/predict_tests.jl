cb86c8cfc06ad05ca7bb3848522e867e695dc3f7
Alister Trabattoni <alister.trabattoni@gmail.com>
2020-04-17 10:53:22 +0200

    Add tests for transition

--- test/predict_tests.jl
+++ test/predict_tests.jl
@@ -7,6 +7,25 @@
         @test move(state, params) == ShipState(2.0, 8.0, 10.0, 5.0, 6.0)
     end
 
+    @testset "transition" begin
+        state = ShipState(2.0, 3.0, 4.0, 5.0, 6.0)
+        ∅ = EmptyState()
+        grid = Grid(
+            range(0.0, 1000.0, length = 5),
+            range(0.0, 10.0, length = 7),
+            range(0.0, 360.0, length = 9),
+            (1:2),
+        )
+        scan = Scan(ones(5), ones(7, 9, 2), grid)
+        params = Parameters(1.0, 0.0, 0.0, 0.0, 1.0)
+        @test transition(state, scan, params) == EmptyState()
+        @test transition(∅, scan, params) == EmptyState()
+        params = Parameters(1.0, 0.0, 1.0, 1.0, 1.0)
+        @test transition(state, scan, params) ==
+              ShipState(2.0, 8.0, 10.0, 5.0, 6.0)
+        @test transition(∅, scan, params) == nothing
+    end
+
     @testset "logf" begin
         state = ShipState(2.0, 3.0, 4.0, 5.0, 6.0)
         ∅ = EmptyState()
