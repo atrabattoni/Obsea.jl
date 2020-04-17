909f1d971408eaef0fc6eb678ae0d8af896eadd7
Alister Trabattoni <alister.trabattoni@gmail.com>
2020-04-17 15:26:16 +0200

    Keep precomputed likelihood in the Scan type

--- src/update.jl
+++ src/update.jl
@@ -7,6 +7,8 @@ end
 
 
 struct Scan
+    y_r::Array{Float64,1}
+    y_fam::Array{Float64,3}
     itp_r::ScaledInterpolation
     itp_fam::ScaledInterpolation
     function Scan(y_r::Array{Float64,1}, y_fam::Array{Float64,3}, grid::Grid)
@@ -23,7 +25,7 @@ struct Scan
         itp_fam = extrapolate(itp_fam, (1.0, Periodic(), Throw()))
         itp_r = scale(itp_r, grid.range_r)
         itp_fam = scale(itp_fam, grid.range_f, grid.range_a, grid.range_m)
-        new(itp_r, itp_fam)
+        new(y_r, y_fam, itp_r, itp_fam)
     end
 end
 
