114623baad97b4fb2df86a5947d530222ee1f21c
Alister Trabattoni <alister.trabattoni@gmail.com>
2020-04-21 10:16:57 +0200

    Continuation...

--- src/range.jl
+++ src/range.jl
@@ -1,19 +1,39 @@
-function tlim(incidence, nmode, depth, celerity)
-    k = collect(1:2:2*nmode+1)
-    t = k .* depth ./ cos(incidence) ./ celerity
+function tlim(incidence, mode, celerity, depth)
+    (2 * mode - 1) * depth / cos(incidence) / celerity
 end
 
 
-function tdoa(r, nmode, depth, celerity, tc, tb)
-    k = collect(1:2:2*nmode+1)
-    t = zeros(nmode + 1)
-    for i = 1:nmode+1
-        t[i] = sqrt((k[i] * depth)^2 + r^2) / celerity
-        if !(tc[i] < t[i] < tb[i])
-            t[i] = NaN
-        end
+function toa(r, mode, celerity, depth, ic, ib)
+    t = sqrt((mode * depth)^2 + r^2) / celerity
+    tc = tlim(ic, mode, celerity, depth)
+    tb = tlim(ib, mode, celerity, depth)
+    if tc < t < tb
+        return t
+    else
+        return NaN
     end
-    tau = diff(t)
+end
+
+
+function tdoa(r, mode, celerity, depth, ic, ib)
+    toa(r, mode + 1, celerity, depth, ic, ib) -
+    toa(r, mode, celerity, depth, ic, ib)
+end
+
+
+function propagation(rrange, nmode, celerity, depth, ic, ib)
+    [tdoa(r, mode, celerity, depth, ic, ib) for r in rrange, mode = 1:nmode]
+end
+
+
+function window(n::Int64, σ::Float64)
+    v = gaussian(n[mode], σ[mode])
+    v /= sum(v)
+end
+
+function window(n::Array{Int64,1}, σ::Array{Float64,1})
+    @assert length(n) = length(σ)
+    Dict(mode => window(n[mode], σ[mode]) for mode = 1:length(n))
 end
 
 
@@ -24,3 +44,42 @@ function convsame(u, v)
     conv(u, v)[1+padding:end-padding]
 end
 
+
+function precomp(z, lam, nmode, v, τrange, τ)
+    u = exp.(-lam ./ 2.0) .* besseli.(0, sqrt.(lam .* z))
+    y = ones(size(τ, 1))
+    for mode = 1:nmode
+        A = convsame(u, v[mode])
+        itp = interpolate!(A, BSpline(Cubic(Line(OnGrid()))))
+        itp = extrapolate(itp, 1.0)
+        itp = scale(itp, τrange)
+        y .*= itp(τ[:, mode])
+    end
+end
+
+
+struct Range
+    nmode::Int64
+    celerity::Float64
+    depth::Float64
+    ic::Float64
+    ib::Float64
+    lam::Float64
+    n::Array{Float64,1}
+    σ::Array{Int64,1}
+    rrange::AbstractRange
+    τrange::AbstractRange
+    v::Dict{Int64,Array{Float64,1}}
+    τ::Array{::Float64,2}
+    function Range(nmode, celerity, depth, ic, ib, lam, n, σ, rrange, τrange)
+        v = windows(n, σ)
+        τ = propagation(rrange, nmode, celerity, depth, ic, ib)
+        new(nmode, celerity, depth, ic, ib, lam, n, σ, rrange, τrange, v, τ)
+    end
+end
+
+
+function precomp(z, model::Range)
+    @unpack z, lam, nmode, n, σ, τrange, τ = model
+    precomp(z, lam, nmode, n, σ, τrange, τ)
+end
--- test/range_tests.jl
+++ test/range_tests.jl
@@ -24,4 +24,29 @@
         @test convsame(ones(5), ones(3)) ≈ [2.0, 3.0, 3.0, 3.0, 2.0]
     end
 
+    @testset "precomp_r" begin
+        import Obsea.precomp_r
+        z = rand(513).^2
+        lam = 1.0
+        nmode = 3
+        range_r = 0.0:100.0:30000.0
+        range_tau = range(0.0, 10.24, length = 513)
+        n = [1, 3, 5]
+        σ = 0.5 / 2.0
+        tc = tlim(deg2rad(30.0), nmode, depth, celerity)
+        tb = tlim(deg2rad(80.0), nmode, depth, celerity)
+        precomp_r(
+            z,
+            lam,
+            nmode,
+            range_r,
+            range_tau,
+            n,
+            σ,
+            depth,
+            celerity,
+            tc,
+            tb,
+        )
+    end
 end
