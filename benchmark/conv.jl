e75a82bfc05254a08452668a7c0a2577fc17828c
Alister Trabattoni <alister.trabattoni@gmail.com>
2020-04-25 17:46:30 +0200

    Even better

--- benchmark/conv.jl
+++ benchmark/conv.jl
@@ -69,11 +69,54 @@ function convsame6(u, v)
     out
 end
 
+function convsame7(u, v)
+    out = zeros(length(u))
+    @inbounds for j = 4:length(out)-3
+        out[j] += u[j-3] * v[1]
+        out[j] += u[j-2] * v[2]
+        out[j] += u[j-1] * v[3]
+        out[j] += u[j] * v[4]
+        out[j] += u[j+1] * v[5]
+        out[j] += u[j+2] * v[6]
+        out[j] += u[j+3] * v[7]
+    end
+    out
+end
+
+
+function convsame8(u, v)
+    pad = length(v) ÷ 2
+    out = zeros(length(u))
+    @avx for i = 1:length(v)
+        for j = 1+pad:length(out)-pad
+            out[j] += u[j+i-pad-1] * v[i]
+        end
+    end
+    out
+end
+
+function convsame9(u, v)
+    pad = length(v) ÷ 2
+    out = Vector{Float64}(undef, length(u))
+    @avx for j = 1+pad:length(out)-pad
+        s = 0.0
+        for i = 1:length(v)
+            s += u[j+i-pad-1] * v[i]
+        end
+        out[j] = s
+    end
+    out
+end
+
+
 @assert convsame2(u, v) ≈ convsame1(u, v)
 @assert convsame3(u, v) ≈ convsame1(u, v)
 @assert convsame4(u, v) ≈ convsame1(u, v)
 @assert convsame5(u, v) ≈ convsame1(u, v)
 @assert convsame6(u, v)[1+3:end-3] ≈ convsame1(u, v)[1+3:end-3]
+@assert convsame7(u, v)[1+3:end-3] ≈ convsame1(u, v)[1+3:end-3]
+@assert convsame8(u, v)[1+3:end-3] ≈ convsame1(u, v)[1+3:end-3]
+@assert convsame9(u, v)[1+3:end-3] ≈ convsame1(u, v)[1+3:end-3]
 
 println()
 println("reference")
@@ -86,10 +129,16 @@ println("inbounds simd")
 @btime convsame4(u, v);
 println("avx")
 @btime convsame5(u, v);
-println("no padding")
+println("avx no padding")
 @btime convsame6(u, v);
+println("inbounds unrolling")
+@btime convsame7(u, v);
+println("avx other order looping")
+@btime convsame8(u, v);
+println("avx accumulator")
+@btime convsame9(u, v);
 println()
 
 @profiler for i = 1:10000
-    convsame6(u, v)
+    convsame9(u, v)
 end
