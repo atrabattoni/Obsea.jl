37405a399892af615cf9ad641d5ba1f8e423658e
Alister Trabattoni <alister.trabattoni@gmail.com>
2020-05-06 10:29:52 +0200

    Fix track test.

--- test/track_tests.jl
+++ test/track_tests.jl
@@ -1,9 +1,9 @@
 import Obsea: track, fetch
 
-@testset "tracker" begin
+@testset "track" begin
     models, propa, grid = parameters("parameters.toml", 50.0, 1024)
     Nt = 10
     zr = zeros(grid.Nτ, Nt)
     za = zeros(grid.Nf, Nt)
-    track(zr, za, "parameters.toml", 50.0, 1024, 100)
+    track(zr, za, models, propa, grid, 100)
 end
