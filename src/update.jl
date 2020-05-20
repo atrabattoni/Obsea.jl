f13fa51c7e65e9545a6c50e8caef02025b60065b
Alister Trabattoni <alister.trabattoni@gmail.com>
2020-05-20 12:48:08 +0200

    Fix update normalization. Put pd inside interpolation.

--- src/update.jl
+++ src/update.jl
@@ -1,23 +1,22 @@
 function update!(weights, cloud, ℓt, models, grid)
-    @unpack pd = models
-    ℓr, ℓa = make(ℓt, grid)
+    ℓ = make(ℓt, models, grid)
     mask = .!isdead.(cloud)
-    @views weights = weights[mask]
     @views cloud = cloud[mask]
     r = sqrt.(cloud.x .^ 2 .+ cloud.y .^ 2)
     f = cloud.f
     a = mod.(atan.(cloud.x, cloud.y), 2π)
     m = cloud.m
-    weights .*= (1.0 .- pd[m]) .+ pd[m] .* ℓr.(r, m) .* ℓa.(f, a, m)
+    weights[mask] .*= ℓ.(r, f, a, m)
     weights ./= sum(weights)
 end
 
-function make(ℓt, grid)
+function make(ℓt, models, grid)
+    @unpack pd = models
     ℓr = interpolate!(ℓt.r, (BSpline(Linear()), NoInterp()))
     ℓa = interpolate!(ℓt.a, (BSpline(Linear()), BSpline(Linear()), NoInterp()))
     ℓr = extrapolate(ℓr, 1.0)
     ℓa = extrapolate(ℓa, 1.0)
     ℓr = scale(ℓr, grid2range(grid.r), 1:grid.Nm)
     ℓa = scale(ℓa, grid2range(grid.f), grid2range(grid.a), 1:grid.Nm)
-    ℓr, ℓa
+    return ℓ(r, f, a, m) = (1.0 .- pd[m]) .+ pd[m] .* ℓr.(r, m) .* ℓa.(f, a, m)
 end
